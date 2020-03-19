18079828323fa5156d2a9f543778921239756437
SriRaparthi <47342564+SriRaparthi@users.noreply.github.com>
2020-03-19 11:04:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 #if __name__ == "__main__":
 #    app.run(host='0.0.0.0',port = '5000')
 
-
 @app.route("/welcomeocr", methods=['POST','GET'])
 def hello_world():
    return "Hello, World!"
